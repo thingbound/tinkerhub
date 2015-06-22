412933c18dba534db39890a051ae9df1035e4aee
Andreas Holstenson <a@holstenson.se>
2015-06-22 20:15:06 +0200

    Convert arguments to actual array
    This fixes issue where arguments was sent as an object over the network.

--- device/public.js
+++ device/public.js
@@ -5,10 +5,12 @@ module.exports = function(device) {
         get: function(proxy, name) {
             if(typeof device[name] !== 'undefined') {
                 return device[name];
+            } else if(name === 'inspect') {
+                return undefined;
             }
 
             return function() {
-                return device.call(name, arguments);
+                return device.call(name, Array.prototype.slice.call(arguments));
             };
         }
     });
