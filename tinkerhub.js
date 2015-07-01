c06cdfdd1772c578e77c897a464b050465fa36b3
Andreas Holstenson <a@holstenson.se>
2015-07-01 21:51:57 +0200

    Removed test device and exposed storage API

--- tinkerhub.js
+++ tinkerhub.js
@@ -6,16 +6,9 @@ network.join();
 
 var registry = require('./device/registry')(network);
 
-registry.register('log:' + network.id, {
-	metadata: {
-		type: 'log'
-	},
-
-	info: function() {
-		console.log.apply(console.log, arguments);
-	}
-});
-
 module.exports = {
-	devices: registry
+	devices: registry,
+
+	storage: storage,
+
 };
