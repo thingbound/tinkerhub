ac2ce2f1952491624f2aeb11a3085acbe6c7b6ee
Andreas Holstenson <a@holstenson.se>
2017-11-26 10:56:57 +0100

    Disabling limits and time APIs as they need work

--- api.js
+++ api.js
@@ -3,8 +3,8 @@
 const Services = require('ataraxia-services');
 const Things = require('./lib/things');
 
-const limits = require('./lib/events/limits');
-const time = require('./lib/utils/time');
+//const limits = require('./lib/events/limits');
+//const time = require('./lib/utils/time');
 
 module.exports = function(network) {
 	const services = new Services(network);
@@ -12,7 +12,7 @@ module.exports = function(network) {
 
 	const result = things.publicApi;
 	Object.defineProperty(result, 'id', { value: network.id });
-	Object.defineProperty(result, 'time', { value: time });
-	Object.defineProperty(result, 'limits', { value: limits });
+	//Object.defineProperty(result, 'time', { value: time });
+	//Object.defineProperty(result, 'limits', { value: limits });
 	return result;
 }
