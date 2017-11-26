634de49d2bdd665d8287f83cd340a5869e7102f4
Andreas Holstenson <a@holstenson.se>
2017-11-26 10:53:19 +0100

    Renaming ServiceLayer to Things

--- api.js
+++ api.js
@@ -1,16 +1,16 @@
 'use strict';
 
 const Services = require('ataraxia-services');
-const ServiceLayer = require('./lib/service-layer');
+const Things = require('./lib/things');
 
 const limits = require('./lib/events/limits');
 const time = require('./lib/utils/time');
 
 module.exports = function(network) {
 	const services = new Services(network);
-	const layer = new ServiceLayer(services, 'core');
+	const things = new Things(services);
 
-	const result = layer.publicApi;
+	const result = things.publicApi;
 	Object.defineProperty(result, 'id', { value: network.id });
 	Object.defineProperty(result, 'time', { value: time });
 	Object.defineProperty(result, 'limits', { value: limits });
