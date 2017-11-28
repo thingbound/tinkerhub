f692c92f82a2b3c2038e4f312ae2c4d8f4f13bd6
Andreas Holstenson <a@holstenson.se>
2017-11-28 16:28:02 +0100

    Adding support for machineId

--- api.js
+++ api.js
@@ -3,6 +3,8 @@
 const Services = require('ataraxia-services');
 const Things = require('./lib/things');
 
+const machineId = require('./lib/machineId');
+
 //const limits = require('./lib/events/limits');
 //const time = require('./lib/utils/time');
 
@@ -12,6 +14,7 @@ module.exports = function(network) {
 
 	const result = things.publicApi;
 	Object.defineProperty(result, 'id', { value: network.id });
+	Object.defineProperty(result, 'machineId', { value: machineId });
 	//Object.defineProperty(result, 'time', { value: time });
 	//Object.defineProperty(result, 'limits', { value: limits });
 	return result;
