a48dd65d4997a0ef8b3cba702e307042ba5628a5
Andreas Holstenson <a@holstenson.se>
2017-11-28 16:31:19 +0100

    Adding mixin for machine details

---
+++ machine-details.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const os = require('os');
+const machineId = require('./lib/machineId');
+const { Thing } = require('abstract-things');
+
+/**
+ * Mixin that updates the identifier and hostname so that they reflect the
+ * machine the service is running on.
+ */
+module.exports = Thing.type(Parent => class extends Parent {
+	init() {
+		return super.init()
+			.then(machineId)
+			.then(id => {
+				if(! this.id) {
+					throw new Error('id must be set to use machine details mixin');
+				}
+
+				this.id = this.id + ':' + id;
+				if(this.metadata.name) {
+					this.metadata.name = this.metadata.name + ' (at ' + os.hostname() + ')';
+				}
+
+				return this;
+			});
+	}
+});
