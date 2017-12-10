cc69ad65f97cff9b7dd592c56df357ac1c404766
Andreas Holstenson <a@holstenson.se>
2017-12-10 16:12:24 +0100

    Adding support for advanced matching and filtering of collections

--- api.js
+++ api.js
@@ -4,6 +4,7 @@ const Services = require('ataraxia-services');
 const Things = require('./lib/things');
 
 const machineId = require('./lib/machineId');
+const matchers = require('./lib/things/matchers');
 
 //const limits = require('./lib/events/limits');
 //const time = require('./lib/utils/time');
@@ -15,6 +16,8 @@ module.exports = function(network) {
 	const result = things.publicApi;
 	Object.defineProperty(result, 'id', { value: network.id });
 	Object.defineProperty(result, 'machineId', { value: machineId });
+
+	Object.defineProperty(result, 'match', { value: matchers });
 	//Object.defineProperty(result, 'time', { value: time });
 	//Object.defineProperty(result, 'limits', { value: limits });
 	return result;
